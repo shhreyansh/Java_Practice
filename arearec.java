793571e5a0da372744511920d910606cd5fe4ee4
Shreyansh Gupta <132381644+shhreyansh@users.noreply.github.com>
2024-07-17 23:11:31 +0530

    Add files via upload

---
+++ arearec.java
@@ -0,0 +1,17 @@
+
+    import java.util.Scanner;
+    public class arearec {
+
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("enter the length ");
+        double length=sc.nextDouble();
+        System.out.println("Enter the breadth");
+        double breadth=sc.nextDouble();
+        double area_of_rectangle=(length*breadth);
+        System.out.println("area of a rectangle is: "+area_of_rectangle);
+    }
+    
+}
+
+    
