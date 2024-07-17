339472b2d76826ba745dafefcfa6354815d658f0
Shreyansh Gupta <132381644+shhreyansh@users.noreply.github.com>
2024-07-17 22:48:22 +0530

    Add files via upload

---
+++ perimetersq.java
@@ -0,0 +1,17 @@
+
+    import java.util.Scanner;
+
+public class perimetersq {
+    public static void main(String[] args) {
+        System.out.println("area of square\n\n");
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the sides of a square");
+        double side=sc.nextDouble();
+        double perimeter=4*side;
+        System.out.println("area of square is: "+perimeter);
+
+    }
+    
+}
+
+
