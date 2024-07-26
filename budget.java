24318469873a74bd6dcd7f695006e9ec82e2673f
Shreyansh Gupta <132381644+shhreyansh@users.noreply.github.com>
2024-07-26 10:21:54 +0530

    Add files via upload

---
+++ budget.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+
+public class budget {
+
+    public static void main(String[] args) {
+           Scanner sc = new Scanner(System.in);
+           System.out.print("enter the budget: ");
+           int budget =sc.nextInt();
+           if(budget>3000){
+            System.out.println("Nainital ghum ke aa sakte ho");
+           }
+           else
+           {
+System.out.println("delhi ghum lo vhi kafi hai ");
+           }
+        }
+        }
